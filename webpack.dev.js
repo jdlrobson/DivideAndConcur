d878267c06ed7f563f1f6b8267bf28d3c6a2a034
jdlrobson <jdlrobson@gmail.com>
2017-11-03 11:23:13 -0700

    Setup webpack, babel
    Used `webpack-cli init`
    and then installed preact

---
+++ webpack.dev.js
@@ -0,0 +1,67 @@
+const webpack = require('webpack');
+const path = require('path');
+
+/*
+ * We've enabled ExtractTextPlugin for you. This allows your app to
+ * use css modules that will be moved into a separate CSS file instead of inside
+ * one of your module entries!
+ *
+ * https://github.com/webpack-contrib/extract-text-webpack-plugin
+ *
+ */
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+
+  output: {
+    filename: '[name].bundle.js',
+    sourceMapFilename: "[name].map",
+    path: path.resolve(__dirname, 'dist/')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+
+        options: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.(less|css)$/,
+
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'less-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ],
+          fallback: 'style-loader'
+        })
+      }
+    ]
+  },
+
+  devtool: 'source-map',
+  resolve: {
+   extensions: ['.js', '.jsx'],
+  },
+
+  plugins: [
+    new ExtractTextPlugin('dist/styles.css.[contentHash].css')
+  ]
+};
