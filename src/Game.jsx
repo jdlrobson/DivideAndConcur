3375c99b45bf1b46113a639c20be00e1a3390b09
jdlrobson <jdlrobson@gmail.com>
2017-11-03 14:25:53 -0700

    Simplify entry point

--- src/Game.jsx
+++ src/Game.jsx
@@ -2,22 +2,39 @@
 import { Component, h } from 'preact';
 import Card from './Card'
 
+function getWords(size) {
+  return fetch('/data/' + size).then((r) => {
+    return r.json();
+  });
+}
+
 export default class Game extends Component {
+  construct() {
+    this.state = {};
+  }
+  componentDidMount() {
+    const setState = this.setState.bind( this );
+    getWords(0).then((dictionary) => {
+      setState( { dictionary } );
+    });
+  }
   render() {
     const props = this.props;
-    const dictionary = props.dictionary;
-    const cards = Object.keys( dictionary ).map((char) => {
+    const dictionary = this.state && this.state.dictionary;
+    const cards = dictionary ? Object.keys( dictionary ).map((char) => {
       return <Card
         className='card'
         key={'card-' + char}
         character={char}
         english={dictionary[char]}
       />;
-    });
+    }) : false;
+    const loader = <div>Loading up!</div>;
+
     return (
       <div className="game">
       <h2>Level {props.level}</h2>
-      {cards}
+      {cards || loader }
       </div>
     );
   }
--- src/main.js
+++ src/main.js
@@ -4,18 +4,10 @@ import Game from './Game'
 // Tell Babel to transform JSX into h() calls:
 /** @jsx h */
 
-function getWords(size) {
-  return fetch('/data/' + size).then((r) => {
-    return r.json();
-  });
-}
-
 (function () {
-  getWords(0).then((dictionary) => {
-    render(
-      <Game level={1} dictionary={dictionary} />,
-      document.getElementById('container')
-    );
-  });
+  render(
+    <Game level={1} />,
+    document.getElementById('container')
+  );
 }());
 
