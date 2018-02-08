75ee7bf25810625b55beca635f92b55d25e88e81
jdlrobson <jdlrobson@gmail.com>
2018-02-07 21:16:12 -0800

    Don't show unknown characters in highlighted

--- src/reducers/highlighted.js
+++ src/reducers/highlighted.js
@@ -1,5 +1,7 @@
 import { makeCardsFromCharacters, dictUtils } from './../helpers/cards'
 
 export function getHighlightedCards(state, char) {
-    return makeCardsFromCharacters(state, dictUtils.decompose(char));
+    return makeCardsFromCharacters(state,
+        dictUtils.decompose(char).filter((char)=>char && char !== '?')
+    );
 }
