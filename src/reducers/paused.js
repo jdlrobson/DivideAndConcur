0692fc6c06109a0a8671b2f5d68af038f3f67d88
jdlrobson <jdlrobson@gmail.com>
2018-02-10 18:00:05 -0800

    Start and end round change state of paused

--- src/reducers/paused.js
+++ src/reducers/paused.js
@@ -2,9 +2,11 @@ import actionTypes from './../actionTypes';
 
 export default (state=false, action) => {
     switch (action.type) {
+        case actionTypes.START_ROUND:
         case actionTypes.FLIP_CARDS_END:
         case actionTypes.DESELECT_ALL_UNANSWERED_CARDS:
             return false;
+        case actionTypes.END_ROUND:
         case actionTypes.FLIP_CARDS_START:
             return true;
         default:
