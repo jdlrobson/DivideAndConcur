3dc294b2d7d37e3edfe30d660ed994d85b4c4dba
jdlrobson <jdlrobson@gmail.com>
2018-04-06 10:59:22 -0700

    Add ui tests with chai and preact-jsx-chai

---
+++ tests/ui/test_Card.jsx
@@ -0,0 +1,35 @@
+import chai, { expect } from 'chai';
+import { Card } from './../../src/ui/Card';
+import assertJsx from 'preact-jsx-chai';
+import { h } from 'preact'; /** @jsx h */
+
+chai.use(assertJsx);
+
+describe('Card', () => {
+    it('if two characters a card is wide', () => {
+        const card = <Card character='国语' isSmall />;
+        expect(
+            card
+        ).to.contain(
+            'card--small'
+        );
+        expect(
+            card
+        ).to.contain(
+            'card--wide'
+        );
+    });
+    it('special haracters with string length greater than 1 are not wide', () => {
+        const card = <Card character='𥫗' isSmall />;
+        expect(
+            card
+        ).to.contain(
+            'card card--small'
+        );
+        expect(
+            card
+        ).to.not.contain(
+            'card--wide'
+        );
+    });
+});
