fd51957921036af399807f9c8a3f9d3a4b85d991
jdlrobson <jdlrobson@gmail.com>
2018-04-07 10:49:04 -0700

    One reducer to rule them all
    The perf reducer is a bit hacky and now triggers a console
    warning, but we'll fix that later.

--- src/reducer.js
+++ src/reducer.js
@@ -1,54 +1,8 @@
 /** @jsx h */
-import { MATCH_SOUND, MATCH_PAIRS, REVISE,
-    ENGLISH_TO_CHINESE, PINYIN_TO_CHINESE,
-    DECK_NEW, DECK_KNOWN, DECK_UNKNOWN,
-} from './constants';
-import { getDifficultyRatings } from './helpers/difficulty-ratings';
-import { shuffle, getSelectedUnansweredCards, getAnsweredCards,
-    dictUtils,
-    makeCardsFromCharacters  } from './helpers/cards';
-import { getUnknownCards, getKnownCards, getHardCards,
-    flipCards, cloneCards, answerCard,
-    selectAndAnswerAll,
-    pickCardsForGame,
-    selectCard, deselectUnansweredCards, markCardsAsAnswered,
-    cutCardDeck, shuffleCards, addIndexToCards } from './reducers/cards';
-import _answers, { markWordAsDifficult, markWordAsEasy } from './reducers/difficulty-ratings';
-import _deck from './reducers/deck';
-import _highlighted from './reducers/highlighted';
-import _isRendered from './reducers/isRendered';
-import _paused from './reducers/paused';
-import _cards from './reducers/cards';
-import _game from './reducers/game';
-import _isFlipping from './reducers/isFlipping';
-import _isFlipped from './reducers/isFlipped';
-import _isDirty from './reducers/isDirty';
-import _isBooted from './reducers/isBooted';
-import _words from './reducers/words';
-import _endRound from './reducers/endRound';
-import actionTypes from './actionTypes';
+import reducers from './reducers';
+import { combineReducers } from 'redux'
 
-const reducer = (state={}, action) => {
-    const highlighted = _highlighted(state.highlighted, action);
-    const paused = _paused(state.paused, action);
-    const isRendered = _isRendered(state.isRendered, action);
-    const deck = _deck(state.deck, action);
-    const isBooted = _isBooted(state.isBooted, action);
-    const words = _words(state.words, action);
-    const answers = _answers(state.answers, action);
-    const cards = _cards(state.cards, action);
-    const isDirty = _isDirty(state.isDirty, action);
-    const game = _game(state.game, action);
-    const endRound = _endRound(state.endRound, action);
-    const isFlipped = _isFlipped(state.isFlipped, action);
-    const isFlipping = _isFlipping(state.isFlipping, action);
-    return Object.assign({}, state, {
-        paused, highlighted, deck, answers, cards,
-        isRendered, isDirty, isBooted, words, game,
-        endRound, isFlipping, isFlipped
-    });
-};
-
+const reducer = combineReducers(reducers);
 const reducerWithPerf = function(state, action) {
     const before = new Date();
     const newState = reducer(state, action);
---
+++ src/reducers/index.js
@@ -0,0 +1,30 @@
+import cards from './cards';
+import answers from './difficulty-ratings';
+import deck from './deck';
+import highlighted from './highlighted';
+import isRendered from './isRendered';
+import paused from './paused';
+import game from './game';
+import isFlipping from './isFlipping';
+import isFlipped from './isFlipped';
+import isDirty from './isDirty';
+import isBooted from './isBooted';
+import words from './words';
+import endRound from './endRound';
+
+export default {
+    cards,
+    answers,
+    deck,
+    highlighted,
+    isRendered,
+    paused,
+    cards,
+    game,
+    isFlipping,
+    isFlipped,
+    isDirty,
+    isBooted,
+    words,
+    endRound
+};
