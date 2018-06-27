2adc49b436bb9a696b45f4996ade69855fb890f0
jdlrobson <jdlrobson@gmail.com>
2018-06-26 22:02:13 -0700

    Drop demo at start when user knows more than 50 words

--- src/reducers/deck.js
+++ src/reducers/deck.js
@@ -1,8 +1,15 @@
 import actionTypes from './../actionTypes';
-import { DECK_START } from './../constants';
+import { DECK_START, DECK_NEW } from './../constants';
+import { getKnownWordCount } from './../helpers/difficulty-ratings';
 
 export default (state=DECK_START, action) => {
     switch (action.type) {
+        case actionTypes.INIT_END:
+            if (getKnownWordCount(action.answers) > 20) {
+                return DECK_NEW;
+            } else {
+                return state;
+            }
         case actionTypes.SET_DECK:
             return action.deck;
         case actionTypes.DISMOUNT_DECK:
