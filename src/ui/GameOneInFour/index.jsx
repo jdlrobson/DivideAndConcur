fd8ec0a6cd2100a3d7d5134539cde32f19aa755c
jdlrobson <jdlrobson@gmail.com>
2018-08-11 08:29:12 -0700

    Make pinyin game less obvious when answer is 2 identical characters
    In the case of 暗暗 it's very obvious what the answer is if the characters
    you are checking against are not identical e.g. in the case of 奄奄 and
    火山 it's very clear the 2nd is not an option

--- src/ui/GameOneInFour/index.jsx
+++ src/ui/GameOneInFour/index.jsx
@@ -8,24 +8,64 @@ import './styles.less';
 
 export const getCardProps = (cardProps, mode, targetCard, isFinished) => {
     let character = cardProps.character;
+    let english = cardProps.english;
     let label = mode === 0 ? cardProps.pinyin : cardProps.english;
     if (mode === 0) {
+        // Please refactor into separate method.
         const targetChar = Array.from(targetCard.character)[0];
         const targetPinyin = targetCard.pinyin.split(' ');
+        const labelSplit = label.split(' ');
         // make sure pinyin is same length as target
-        if (targetPinyin.length > label.split(' ').length) {
-            label = `${targetPinyin[0]} ${label}`;
-            character = targetChar + character;
-        } else if (targetPinyin.length < label.split(' ').length) {
-            label = label.split(' ')[0];
+        if (targetPinyin.length > labelSplit.length) {
+            // If the target is 2 characters and this is one, we'll want to update this one
+            // to be a worthy answer (otherwise its obvious which is the correct answer)
+            if (targetPinyin[0] === targetPinyin[1]) {
+                // in this case we have something like 暗暗 so let's double this label
+                // e.g. 水 becomes 水水
+                label = `${label} ${label}`;
+                character += character;
+            } else {
+                // in this case we have something like 火山 where the characters are different
+                // e.g. 水 becomes 水山
+                label = `${targetPinyin[0]} ${label}`;
+                character = targetChar + character;
+            }
+            // Since this is no longer a real word, reset english
+            english = '';
+        } else if (targetPinyin.length < labelSplit.length) {
+            // Here we have the case where the answer is 水 but we have a card 火山
+            // 火山 becomes 火
+            label = labelSplit[0];
             character = Array.from(character)[0];
+            // Since this /might/ no longer a real word, reset english
+            english = '';
+        } else if (targetPinyin.length === labelSplit.length) {
+            const targetDupeCharacters = targetPinyin[0] === targetPinyin[1];
+            const answerDupeCharacters = labelSplit[0] === labelSplit[1];
+
+            // If the solution is 暗暗 and one of possible answers is 暗暗
+            // characters of the answer are different
+            if (!answerDupeCharacters && targetDupeCharacters) {
+                label = labelSplit[0] + ' ' + labelSplit[0];
+                character = Array.from(character)[0];
+                character += character;
+                // Since this /might/ no longer a real word, reset english
+                 english = '';
+            } else if ( answerDupeCharacters && !targetDupeCharacters ) {
+                // If correct answer is 火山 and one of answers is 暗暗 turn 暗暗 into a viable answer
+                label = labelSplit[0] + ' ' + targetPinyin[0];
+                character = Array.from(character)[0] + targetChar;
+                // Since this /might/ no longer a real word, reset english
+                english = '';
+            }
         }
     }
     const modeBasedCardData = mode === 0 ?
-        { hideEnglish: true, pinyin: label, character } :
+        { hideEnglish: true, pinyin: label, character, english } :
         { hidePinyin: true,
             character,
             english: label };
+
     return Object.assign({}, cardProps, {
         hideCharacter: !cardProps.isAnswered,
         className: 'game-one-four__choices__card',
