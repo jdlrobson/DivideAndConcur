3b6af03057a91e8267a19523b5b0e453d3b86461
jdlrobson <jdlrobson@gmail.com>
2018-11-29 21:04:23 -0800

    Generalise build file

--- build-html.js
+++ build-html.js
@@ -4,18 +4,34 @@ import fs from 'fs';
 import render from 'preact-render-to-string';
 import StoryPanel from './src/ui/StoryPanel';
 
-const FILENAME = 'index';
-
 /** @jsx h */
 
-const buf = fs.readFileSync(FILENAME + '.html.template', 'utf8');
+const buf = fs.readFileSync('index.html.template', 'utf8');
 
-const doSubstitution = ( input ) => {
-    let panelHtml = render(
-        <StoryPanel />
-    );
-    return input.replace(/(<!-- STORY-PANEL-START -->)(.*?)(<!-- STORY-PANEL-END -->)/g, '$1' + panelHtml + '$3')
-}
-fs.writeFile(FILENAME + '.html', doSubstitution(buf), 'utf8', (error) => {
-    console.log( error ? 'Error occurred' + error : 'Wrote html without error' );
-});
+const doSubstitution = (html) => {
+    return buf.replace(/(<!-- STORY-PANEL-START -->)(.*?)(<!-- STORY-PANEL-END -->)/g,
+        `$1${html}$3`);
+};
+
+const saveFile = (filename, text) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filename, doSubstitution(text), 'utf8', (error) => {
+            if (error) {
+                console.log(`Error occurred ${error}`);
+                reject(error);
+            } else {
+                resolve();
+            }
+        });
+    } );
+};
+
+const promises = [];
+
+// index.html
+promises.push(
+    saveFile(
+        'index.html',
+        render(<StoryPanel />)
+    )
+);
