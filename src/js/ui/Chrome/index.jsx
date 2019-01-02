b1dbe63b0d5b70652d45fecdbbf757fed7f711e8
jdlrobson <jdlrobson@gmail.com>
2019-01-01 17:27:10 -0800

    JSX Style changes

--- src/js/ui/Chrome/index.jsx
+++ src/js/ui/Chrome/index.jsx
@@ -5,7 +5,7 @@ const Header = ({ logo }) => {
     return (
         <div className='chrome__header'>
             <div className='chrome__header__home'>
-                <a href="/">
+                <a href='/'>
                     <img src={logo} alt='Divide and concur'
                         width='320'
                         className='chrome__header__home__logo' />
@@ -17,7 +17,8 @@ const Header = ({ logo }) => {
 
 const ChromeContent = ( { children, name} ) => {
     return (
-        <div className="chrome__content" id={"chrome__content__panel-" + name}>
+        <div className='chrome__content'
+            id={`chrome__content__panel-${name}`}>
             { children }
         </div>
     );
@@ -27,19 +28,18 @@ export default ({ logo, children }) => {
     return (
         <div className='chrome'>
             <Header logo={logo} />
-            <ChromeContent name="one">
+            <ChromeContent name='one'>
                 {children}
             </ChromeContent>
-            <div id="container" class="chrome__content"  style="display: none;">
-                <ChromeContent name="two" />
+            <div id='container' class='chrome__content'  style='display: none;'>
+                <ChromeContent name='two' />
             </div>
             <footer>
                 <p>
-                    <a href="http://jdlrobson.com">Jon Robson and
-                    Linz Lim's</a>&nbsp;
-                    <a href="https://qieqie.uk">qie qie</a> a fun way to learn Mandarin
-                    <span id="version">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                    <span id="modified"></span>
+                    <a href='http://jdlrobson.com'>Jon Robson and Linz Lim's</a>&nbsp;
+                    <a href='https://qieqie.uk'>qie qie</a> a fun way to learn Mandarin
+                    <span id='version'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                    <span id='modified' />
                 </p>
             </footer>
         </div>
